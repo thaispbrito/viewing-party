d1427fca6a2458573edcf66fe989d720d9e249b4
Thais Palma de Brito <britop.tech@gmail.com>
2026-09-15 21:01:31 -0700

    Implement create_movie, add_to_watched, add_to_watchlist, and watch_movie functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,39 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+
+    if not title or not genre or not rating:
+        return None
+
+    new_movie = {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+
+    user_data["watched"].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            add_to_watched(user_data,movie)
+            break
+
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
